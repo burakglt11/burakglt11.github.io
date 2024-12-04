7e7f3cf08f83822a453aa5acd43338a34ec1ff0d
BURAK GÜLTEKİN <burakgultekin781@gmail.com>
2024-12-04 18:28:47 +0300

    Create style.h
    ilk css kodum

---
+++ style.h
@@ -0,0 +1,356 @@
+
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  background: url(2.jpg.png);
+  height: 100vh;
+  background-size: 100% 100%;
+}
+
+.container .navbar {
+  width: 100%;
+  height: 80px;
+  background:#ff6e00 );
+}
+
+.navbar.logo {
+  display: inline-block;
+  margin-left: 50px;
+  margin-top: 20px;
+}
+
+.navbar.logo a {
+  text-decoration: none;
+  font-size: 30px;
+  font-family: sans-serif;
+  color: bisque;
+}
+
+.navbar ul {
+  list-style: none;
+  display: inline-block;
+  margin: 0 8px;
+  line-height: 80px;
+}
+
+.navbar ul li {
+  display: inline-block;
+  margin: 0 8px;
+}
+
+.navbar ul li a {
+  color:black;
+  text-decoration: none;
+  font-size: 20px;
+  padding: 6px 13px;
+}
+
+.navbar ul li a.active,
+.navbar ul li a:hover {
+  background: #ff6e00;
+  border-radius: 2px;
+}
+
+.navbar ul {
+  float: right;
+  margin-right: 20px;
+}
+
+.container.center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: sans-serif;
+  user-select: none;
+}
+
+.center h1 {
+  color: black;
+  font-size: 70px;
+  font-weight: bold;
+  width: 900px;
+  text-align: center;
+}
+
+.center h2 {
+  color: black;
+  font-size: 50px;
+  font-weight: bold;
+  width: 885px;
+  margin-top: 10px;
+  text-align: center;
+}
+
+.center .buttons {
+  margin: 35px 280px;
+}
+
+.buttons button {
+  height: 50px;
+  width: 150px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #ffb3b3;
+  background: rgb(207, 96, 88);
+  border: solid 1px red;
+  cursor: pointer;
+  outline: none;
+  border-radius: 25px;
+}
+
+.buttons button:hover {
+  background: #ffff;
+}
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+}
+
+.container {
+  text-align: center;
+}
+
+h1 {
+  color: #333;
+}
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+}
+
+.container {
+  text-align: center;
+  margin-top: 50px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+h1 {
+  color: #333;
+}
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  margin: 0;
+}
+
+.navbar {
+  background-color: gray; /* Mavi renk */
+  padding: 10px 0;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+.navbar ul li {
+  display: inline;
+  margin: 0 15px;
+}
+
+.navbar ul li a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.container {
+  text-align: center;
+  margin-top: 50px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+h1 {
+  color: #333;
+}
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  margin: 0;
+}
+
+.header {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.header h2 {
+  color: black;
+  font-size: 36px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+.navbar {
+  background-color: gray; /* Mavi renk */
+  padding: 10px 0;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+.navbar ul li {
+  display: inline;
+  margin: 0 15px;
+}
+
+.navbar ul li a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.container {
+  text-align: center;
+  margin-top: 50px; /* Bu değeri ihtiyacınızabody {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+}
+
+.header {
+    text-align: center;
+    margin-top: 20px;
+}
+
+.header h2 {
+    color: black;
+    font-size: 36px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+.navbar {
+    background-color: #808080; /* Gri renk */
+    padding: 10px 0;
+}
+
+.navbar ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
+
+.navbar ul li {
+    display: inline;
+    margin: 0 15px;
+}
+
+.navbar ul li a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.container {
+    text-align: center;
+    margin-top: 50px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+h1 {
+    color: #333;
+}
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  margin: 0;
+}
+
+.header {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.header h2 {
+  color: black;
+  font-size: 36px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+.navbar {
+  background-color: #808080; /* Gri renk */
+  padding: 10px 0;
+}
+
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+.navbar ul li {
+  display: inline;
+  margin: 0 15px;
+}
+
+.navbar ul li a {
+  color: white;
+  text-decoration: nonebody {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+.header {
+    text-align: center;
+    margin-top: 20px;
+}
+
+.header h2 {
+    color: black;
+    font-size: 36px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+.navbar {
+    background-color: #808080; /* Gri renk */
+    padding: 10px 0;
+    width: 100%;
+    text-align: center;
+}
+
+.navbar ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+.navbar ul li {
+    display: inline;
+    margin: 0 15px;
+}
+
+.navbar ul li a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.container {
+    text-align: center;
+    margin-top: 50px; /* Bu değeri ihtiyacınıza göre ayarlayabilirsiniz */
+}
+
+h1 {
+    color: black; /* Siyah renk */
+}
